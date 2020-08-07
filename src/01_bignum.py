ed0ec3f744b558635ecf998a279cb20c1769743a
littleonetwo <littleonetwo@gmail.com>
2020-08-07 18:04:34 -0500

    some of the assignment

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,9 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+
+value = 2
+power = 65536
+
+print(value**power)
--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,6 +35,9 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints.append({"lat": 44, "lon": 80, "name": "a new place"})
+
+print(waypoints)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
@@ -42,6 +45,13 @@ waypoints = [
 # waypoints list.
 
 # YOUR CODE HERE
+waypoints[0]["name"]="not a real place"
+waypoints[0]["lon"]= -130
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+
+for x in range(len(waypoints)):
+  for a, b in waypoints[x].items():
+    print(a, b)
